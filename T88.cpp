e6fbe1f8c76424ef29fd32d022b6ce57d2c428a3
Wallfacerdl <Wallfacerdl@gmail.com>
2025-12-05 14:43:50 +0800

    12.5初始

--- T88.cpp
+++ T88.cpp
@@ -1,5 +1,6 @@
 // 88. 合并两个有序数组
 #include <vector>
+#include <iostream>
 using namespace std;
 
 class Solution
@@ -7,16 +8,18 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
+        if (nums2.empty())
+            return;
         int i = 0, j = 0;
         int offset = 0;
-        while (offset < n)
+        while (i < m)
         {
-            if (nums1[i] < nums2[j])
+            if (nums1[i] <= nums2[j])
             {
                 i++;
                 continue;
             }
-            else if (nums1[i] > nums2[j])
+            while (nums1[i] > nums2[j] && i < m)
             {
                 nums1[m + offset] = nums1[i];
                 nums1[i] = nums2[j];
@@ -25,9 +28,25 @@ public:
                 i++;
             }
         }
+        while (offset < n)
+        {
+            nums1[m + offset] = nums2[j];
+            offset++;
+            j++;
+        }
     }
 };
 int main()
 {
     // [1,2,3,0,0,0]
+    Solution sol;
+    vector<int> x{2, 0};
+    vector<int> y{1};
+    int x_size = 1;
+    int y_size = y.size();
+    sol.merge(x, x_size, y, y_size);
+    for (int &num : x)
+    {
+        cout << num << " ";
+    }
 }
