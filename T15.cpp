c5d8cca2eb1d5c1bbd83517afa472f8c4f69b1ce
WallFacerDl <wallfacerdl@gmail.com>
2026-02-01 21:33:00 +0800

    完善同步功能

--- T15.cpp
+++ T15.cpp
@@ -3,45 +3,68 @@
 #include <iostream>
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> threeSum(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
         int n = nums.size();
-        for (int i = 0;i<n;i++){
+        for (int i = 0; i < n; i++)
+        {
             // cout << "i " << i << endl;
-            if (i > 0 && nums[i-1] == nums[i]) continue;
-            int sum = - nums[i];
-            int start = i + 1,end = n - 1;
-            while(start < end)
+            if (i > 0 && nums[i - 1] == nums[i])
+                continue;
+            int sum = -nums[i];
+            int start = i + 1, end = n - 1;
+            while (start < end)
             {
-                if (nums[start] + nums[end] < sum){
-                    start ++;
-                    while(nums[start] == nums[start - 1]){
+                if (nums[start] + nums[end] < sum)
+                {
+                    start++;
+                    while (nums[start] == nums[start - 1])
+                    {
                         start++;
-                        if (start>=end){
+                        if (start >= end)
+                        {
                             break;
                         }
                     }
                 }
-                else if (nums[start] + nums[end] > sum ){
-                    end --;
-                    while(nums[end] == nums[end + 1]){
-                        end --;
-                        if (end <= i ) break;
+                else if (nums[start] + nums[end] > sum)
+                {
+                    end--;
+                    while (nums[end] == nums[end + 1])
+                    {
+                        end--;
+                        if (end <= i)
+                            break;
                     }
                 }
-                else{
+                else
+                {
                     // -4 -1 -1 0 1 2
-                    vector <int> v0 = {nums[i],nums[start],nums[end]};
+                    vector<int> v0 = {nums[i], nums[start], nums[end]};
                     result.push_back(v0);
                     // cout << "内部"<< start << " " << end << endl;
-                    start ++;end --;
-                    while(nums[start] == nums[start - 1]) {start++;if(start >= n) break;}
-                    while(nums[end] == nums[end + 1]) {end --;if(end <= i) break;}
+                    start++;
+                    end--;
+                    while (nums[start] == nums[start - 1])
+                    {
+                        start++;
+                        if (start >= n)
+                            break;
+                    }
+                    while (nums[end] == nums[end + 1])
+                    {
+                        end--;
+                        if (end <= i)
+                            break;
+                    }
                 }
             }
         }
