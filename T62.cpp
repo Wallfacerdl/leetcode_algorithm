8ed3270812f490b8a606de8eacc519203dbf82eb
WallFacerDl <wallfacerdl@gmail.com>
2026-03-06 10:18:20 +0800

    T62 法二：动态规划（未完成）

--- T62.cpp
+++ T62.cpp
@@ -1,8 +1,10 @@
 // T62:不同路径 # 动态规划 # 组合数
-
+#include<iostream>
+using namespace std;
+#include <vector>
 
 // 法一：组合数
-class Solution {
+class Solution1 {
 public:
     int uniquePaths(int m, int n) {
         m = m - 1; // 组合数上标
@@ -17,7 +19,7 @@ public:
     }
 };
 // 法二： 动态规划
-class Solution {
+class Solution2 {
 public:
     int uniquePaths(int m, int n) {
         // 初始化
@@ -27,16 +29,27 @@ public:
         while (row <= m+ n - 2)
         {
             row ++;
-            cout << row << endl;
+            // cout << row << endl;
             for (int i = row,j = 0;i>=0 && i< n && j >=0 && j <m;i--,j++)
             {
                 if (i == 0) mat[i][j] = 1;
                 else if (j == 0) mat[i][j] = 1;
                 else
+                {
                     mat[i][j] = mat[i-1][j] + mat[i][j-1];
+                    cout << mat[i][j] << " ";
+                }
             }
         }
         return mat[i][j];
 
     }
 };
+int main()
+{
+    // 法一
+    // Solution1 sol1;
+    // cout << sol1.uniquePaths(3,7) << endl;
+    Solution2 sol2;
+    cout << sol2.uniquePaths(3,7) << endl;
+}
